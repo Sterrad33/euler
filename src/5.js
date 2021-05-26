fb3b1a34646941ab96eb10dc90a0a089c4fb4bad
Moni <40552237+NotMoni@users.noreply.github.com>
2021-05-26 10:08:28 -0400

    added #5
    Signed-off-by: Moni <40552237+NotMoni@users.noreply.github.com>

--- src/5.js
+++ src/5.js
@@ -1,3 +1,22 @@
 // https://projecteuler.net/problem=5
 // Smallest multiple
 
+function isEvenly(num) {
+	for (let i = 2; i <= 21; i += 1) {
+    if (num % i !== 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+let noomber = 20
+
+while(true) {
+  if(isEvenly(noomber)) {
+    break;
+  }
+  noomber += 20;
+}
+
+console.log(noomber)
